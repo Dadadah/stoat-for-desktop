da9360567d1ce11dbe8f2c948e4f66c9a0780cba
Jacob Schlecht <dadadah@echoha.us>
2026-08-02 19:09:22 -0600

    feat: Add virtual microphone for linux screen share
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig } from "vite";
+import nativePlugin from "vite-plugin-native-modules";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [nativePlugin()],
+  build: {
+    commonjsOptions: {
+      include: [/node-pipewire/, /node_modules/],
+    },
+  },
+});
